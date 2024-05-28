ef735a256be001842761b97c38bae6b841483665
lundqvisthampus <lundqvisthampus@outlook.com>
2024-05-28 14:32:35 +0200

    Authorization added

--- VerificationProvider/Functions/ValidateVerificationCode.cs
+++ VerificationProvider/Functions/ValidateVerificationCode.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using VerificationProvider.Services;
 
 namespace VerificationProvider.Functions
@@ -14,6 +18,24 @@ namespace VerificationProvider.Functions
         [Function("ValidateVerificationCode")]
         public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "validate")] HttpRequest req)
         {
+            if (!req.Headers.TryGetValue("Authorization", out var authHeader))
+            {
+                return new UnauthorizedResult();
+            }
+
+
+            var token = authHeader.FirstOrDefault()?.Split(" ").Last();
+            if (string.IsNullOrEmpty(token))
+            {
+                return new UnauthorizedResult();
+            }
+
+            if (!ValidateToken(token, out ClaimsPrincipal claimsPrincipal))
+            {
+                return new UnauthorizedResult();
+            }
+        
+
             try
             {
                 var request = await _validateCodeService.UnpackRequestAsync(req);
@@ -32,6 +54,33 @@ namespace VerificationProvider.Functions
             }
 
             return new UnauthorizedResult();
+        }
+
+        private static bool ValidateToken(string token, out ClaimsPrincipal claimsPrincipal)
+        {
+            claimsPrincipal = null;
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("3e0f2db8-3ce3-4e22-b53a-7a609b4b7048");
+
+            try
+            {
+                claimsPrincipal = tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = "TokenProvider",
+                    ValidateAudience = true,
+                    ValidAudience = "Silicon",
+                    ValidateLifetime = true,
+                }, out SecurityToken validatedToken);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
